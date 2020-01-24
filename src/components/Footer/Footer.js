6c3c9cf804da916022a4dde5a4fb055d45ca9dba
ranausama48 <ranausama48@gmail.com>
2020-01-24 18:49:38 +0500

    Update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,58 +1,82 @@
-import React, { Component } from 'react'
-import footer from '../Images/footer.jpg'
-import { Container, Row, Col  } from 'reactstrap';
-import { FaFacebookSquare, FaWeight } from "react-icons/fa";
-import { FaYoutubeSquare } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import Zoom from 'react-reveal/Zoom';
-import './style.css'
+import React, { Component } from "react";
+import { FaLinkedinIn, FaGithub, FaTwitter, FaFacebookF } from "react-icons/fa";
+import Zoom from "react-reveal/Zoom";
+import Bounce from "react-reveal/Bounce";
+import "./style.css";
 class Footer extends Component {
   render() {
     return (
-      <div>
-            <div style={{backgroundColor:'grey',padding:'20px 0px'}} >
-               <div className="" >
-                <Row className="container-fluid">
-                        <Col lg="3" md="3" md="12" xs="12" >
-                            <div className="text-center" style={{fontWeight:'bold',color:'white'}}>
-                            <Zoom>
-                                <h3 className="text-center">
-                                    FashionIcon 
-                                </h3>   
-                                </Zoom> 
-                            </div>
-                            
-                        </Col>
-                        <Col lg="6" md="6" sm="12" xs="12"  >
-                            <Zoom>
-                                <div className="text-center">
-                                    <a target="_blank" href="https://www.facebook.com/Vom-Wild-Wolf-House-444549682947403/?ref=br_rs"><span className="footer_icons"><FaFacebookSquare/></span></a>
-                                    <a target="_blank" href="https://www.youtube.com/channel/UC_JV8HHg0bgo4lsY7b5o-EQ/featured"><span className="footer_icons"><FaYoutubeSquare/></span></a>
-                                    <a target="_blank" href="https://www.youtube.com/channel/UC_JV8HHg0bgo4lsY7b5o-EQ/featured"><span className="footer_icons"><FaInstagram/></span></a>
-                                    <a target="_blank" href="https://www.youtube.com/channel/UC_JV8HHg0bgo4lsY7b5o-EQ/featured"><span className="footer_icons"><FaFacebookSquare/></span></a>
-                                    <a target="_blank" href="https://www.youtube.com/channel/UC_JV8HHg0bgo4lsY7b5o-EQ/featured"><span className="footer_icons"><FaLinkedin/></span></a>
-                                </div>
-                            </Zoom>
-                        </Col>
-                        <Col lg="3" md="3" md="12" xs="12"  >
-                            <div className="text-center" style={{fontWeight:'bold',color:'white'}}>
-                           
-                                <span className="text-center">
-                                    <Zoom>
-                                        Developed By OsamaRajpoot 
-                                    </Zoom>
-                                </span>
-                                    
-                            </div>
-                        </Col>
-                    </Row>
-               </div>
-            </div>
-        {/* <img src={footer}/> */}
+      <div
+        style={{
+          backgroundColor: "grey",
+          padding: "15px 0px"
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center"
+          }}
+        >
+          <Bounce left>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/usamaasif48/"
+              className="a-social_icon"
+            >
+              <span className="footer_icons">
+                <FaLinkedinIn />
+              </span>
+            </a>
+          </Bounce>
+          <Bounce left>
+            <a
+              target="_blank"
+              href="https://github.com/ranausama48"
+              className="a-social_icon"
+            >
+              <span className="footer_icons">
+                <FaGithub />
+              </span>
+            </a>
+          </Bounce>
+          <Bounce left>
+            <a
+              target="_blank"
+              href="https://twitter.com/ranausamaasif"
+              className="a-social_icon"
+            >
+              <span className="footer_icons">
+                <FaTwitter />
+              </span>
+            </a>
+          </Bounce>
+          <Bounce left>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/"
+              className="a-social_icon"
+            >
+              <span className="footer_icons">
+                <FaFacebookF />
+              </span>
+            </a>
+          </Bounce>
+        </div>
+        <div>
+          <p
+            style={{
+              textAlign: "center",
+              marginTop: "10px",
+              marginBottom: "0px"
+            }}
+          >
+            FashionIcon &copy; 2020
+          </p>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Footer
+export default Footer;
